8263fcf4179a1565b79bcea46db699dbf1994763
Gio <giovanni.lodi42@gmail.com>
2014-06-15 01:06:31 +0100

    Setup jade

--- build.js
+++ build.js
@@ -1,8 +1,12 @@
 var metalsmith  = require("metalsmith")
+    , jade      = require("metalsmith-jade")
 
 metalsmith(__dirname)
+  .use(jade())
+
   .source("src")
   .destination("public")
+
   .build(function(err) {
         if (err) throw err;
     });
