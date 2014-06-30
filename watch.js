ee0595e1a98ef8d18e31000da255d3bdff1675c7
Gio <giovanni.lodi42@gmail.com>
2014-06-30 22:39:56 +0100

    Watch script recompiles when package.json changes + syntax fixes

--- watch.js
+++ watch.js
@@ -1,12 +1,12 @@
 var watch = require('node-watch');
 var exec = require('child_process').exec;
 
-function puts(error, stdout, stderr) { 
+function puts(error, stdout, stderr) {
 	if (stdout) { console.log(stdout); }
 	if (stderr) { console.log(stderr); }
 }
 
-watch(['build.js', 'src'], function(filename) {
+watch(['build.js', 'src', 'package.json'], function(filename) {
 	console.log(filename, 'changed. Re-buildind...');
-	exec("node build.js", puts);  
+	exec("node build.js", puts);
 });
